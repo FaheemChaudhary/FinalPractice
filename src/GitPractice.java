eafe56010b71399f54d6521084d538ba4d7347f7
Faheem <fame2845@gmail.com>
2021-05-25 00:46:34 -0400

    Git final commit

---
+++ src/GitPractice.java
@@ -0,0 +1,9 @@
+
+public class GitPractice {
+//Delete all letters
+	public static void main(String[] args) {
+			String st="x12y45z67";
+			st=st.replaceAll("[0-9]", "");
+	System.out.println(st);
+	}
+}
